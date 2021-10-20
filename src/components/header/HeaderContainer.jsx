22f787cba443daccabe5410d71e674780eee1cfb
Berserker04 <xcarlosdesarrolladorx@gmail.com>
2021-10-20 10:33:52 -0500

    Initial config

---
+++ src/components/header/HeaderContainer.jsx
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { NavLink } from "react-router-dom";
+
+// import logo from "../../assets/images/logo.png";
+import "./styles.css";
+
+class HeaderContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="header-container">
+        <div className="header-logo">
+          <h1>InfoTeck</h1>
+          <figure>
+            <NavLink exact to="/">
+              {/* <img height={80} width={80} src={logo} alt="" /> */}
+            </NavLink>
+          </figure>
+        </div>
+
+        <div className="header-links">
+          <NavLink
+            exact
+            className="header-module-desactive"
+            activeClassName="header-module-active"
+            to="/"
+          >
+            Inicio
+          </NavLink>
+          {this.props.getUser.role_id.name === "superUser" && (
+            <NavLink
+              className="header-module-desactive"
+              activeClassName="header-module-active"
+              to="/usuarios"
+            >
+              Gestión de usuario
+            </NavLink>
+          )}
+          <NavLink
+            className="header-module-desactive"
+            activeClassName="header-module-active"
+            to="/publicaciones"
+          >
+            Publicaciones
+          </NavLink>
+          <NavLink
+            className="header-module-desactive"
+            activeClassName="header-module-active"
+            to="/reportes"
+          >
+            Reportes
+          </NavLink>
+          {this.props.getUser.role_id.name === "superUser" && (
+            <NavLink
+              className="header-module-desactive"
+              activeClassName="header-module-active"
+              to="/roles"
+            >
+              Roles
+            </NavLink>
+          )}
+          <NavLink
+            className="header-module-desactive"
+            activeClassName="header-module-active"
+            to="/perfil"
+          >
+            Perfil
+          </NavLink>
+          <a
+            className="header-logout"
+            href="/login"
+            onClick={() => localStorage.removeItem("token")}
+          >
+            Cerrar sesión
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    getUser: state.usersReducer.user,
+  };
+};
+
+const mapDispatchToProps = (Dispatch) => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(HeaderContainer);
---
+++ src/routes/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./styles.css";
+
+import HeaderContainer from "../components/header/HeaderContainer";
+import CheckAuth from "../pages/login/CheckAuth";
+import LoginContainer from "../pages/login/LoginContainer";
+import RegisterContainer from "../pages/login/RegisterContainer";
+// import HomeContainer from "../pages/home/HomeContainer";
+// import UserContainer from "../pages/user/UserContainer";
+// import PublicationContainer from "../pages/publication/PublicationContainer";
+// import ReportContainer from "../pages/report/ReportContainer";
+// import ProfileContainer from "../pages/profile/ProfileContainer";
+// import RoleContainer from "../pages/role/RoleContainer";
+import NoFound from "../pages/noFound";
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/registrar" component={RegisterContainer} />
+      <Route exact path="/login" component={LoginContainer} />
+    </Switch>
+    <CheckAuth>
+      <div className="dashboard-header">
+        <HeaderContainer />
+        <Switch>
+          {/* <Route exact path="/" component={HomeContainer} />
+          <Route exact path="/usuarios" component={UserContainer} />
+          <Route exact path="/publicaciones" component={PublicationContainer} />
+          <Route exact path="/reportes" component={ReportContainer} />
+          <Route exact path="/roles" component={RoleContainer} />
+          <Route exact path="/perfil" component={ProfileContainer} /> */}
+          <Route component={NoFound} />
+        </Switch>
+      </div>
+    </CheckAuth>
+  </BrowserRouter>
+);
+
+export default App;
