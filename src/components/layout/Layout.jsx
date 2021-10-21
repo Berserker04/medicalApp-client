d5bf3ae6fd4a38fc7934a6b498deccbdc096c4a4
jose julian <josejuliandlf@gmail.com>
2021-10-20 19:40:44 -0500

    new views

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -10,9 +10,10 @@ const Layout = ({children}) => {
             <div className="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg ps ps--active-y">
                 <Navbar/>
                 <div className="container-fluid py-4">   
-                    {/* <Employees/> */}
                     {children}
                 </div>
+               {/*  <div className="ps__rail-x" style={{left: "0px", bottom: "0px"}}><div className="ps__thumb-x" tabindex="0" style={{left: "0px", width: "0px"}}></div></div>
+                <div className="ps__rail-y" style={{top: "0px", height: "625px" ,right: "0px"}}><div className="ps__thumb-y" tabindex="0" style={{top: "0px", height: "116px"}}></div></div> */}
             </div>
         </>
     );
